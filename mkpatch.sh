958f415f551a3101cf279ed8dcc5930a88cc059c
Jilles Bakker <bakkerjilles@gmail.com>
2021-12-25 23:20:05 +0100

    update mkpatch utility

--- mkpatch.sh
+++ mkpatch.sh
@@ -2,7 +2,7 @@
 
 FILES="Makefile config.mk config.def.h arg.h st.1 st.c st.desktop \
 	st.h st.info x.c win.h exception.h"
-DIFF=$(git diff master custom $FILES)
+DIFF=$(git diff upstream/master master $FILES)
 
 echo "$(sha256sum <<< $DIFF)" 1>&2
 echo "$DIFF"
