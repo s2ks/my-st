d6422d33c03bdb76d8b8f29597f31f77b8d7b9a3
Jilles Bakker <bakkerjilles@gmail.com>
2021-12-26 00:11:34 +0100

    compare against version 0.8.4 rather than remote master branch

--- mkpatch.sh
+++ mkpatch.sh
@@ -2,7 +2,7 @@
 
 FILES="Makefile config.mk config.def.h arg.h st.1 st.c st.desktop \
 	st.h st.info x.c win.h exception.h"
-DIFF=$(git diff upstream/master master $FILES)
+DIFF=$(git diff 0.8.4 master $FILES)
 
 echo "$(sha256sum <<< $DIFF)" 1>&2
 echo "$DIFF"
